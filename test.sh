b23f9896a0335a89f9615b271a1a4b63aaf295ad
TJ Draper <tdraper@rxante.com>
2025-01-09 09:52:18 -0600

    Add tests in PHP 8.4 environment

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -z "$(docker images -q phpdateimmutable_php82 2> /dev/null)" ]; then
+if [ -z "$(docker images -q phpdateimmutable_php84 2> /dev/null)" ]; then
     docker build \
         --build-arg BUILDKIT_INLINE_CACHE=1 \
-        --cache-from phpdateimmutable_php82 \
-        --file tests/Docker/php82/Dockerfile \
-        --tag phpdateimmutable_php82 \
+        --cache-from phpdateimmutable_php84 \
+        --file tests/Docker/php84/Dockerfile \
+        --tag phpdateimmutable_php84 \
         .;
 fi
 
@@ -17,6 +17,20 @@ if [ -z "$(docker images -q phpdateimmutable_php83 2> /dev/null)" ]; then
         --tag phpdateimmutable_php83 \
         .;
 fi
+
+if [ -z "$(docker images -q phpdateimmutable_php82 2> /dev/null)" ]; then
+    docker build \
+        --build-arg BUILDKIT_INLINE_CACHE=1 \
+        --cache-from phpdateimmutable_php82 \
+        --file tests/Docker/php82/Dockerfile \
+        --tag phpdateimmutable_php82 \
+        .;
+fi
+
+docker run -it --rm \
+    -v "$(pwd):/app" \
+    -w /app \
+    phpdateimmutable_php84 bash -c "php -d memory_limit=4G vendor/bin/pest --fail-on-warning";
 
 docker run -it --rm \
     -v "$(pwd):/app" \
